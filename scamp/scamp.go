fb8e03332942d1731a3e6e1b76387b148cc00388
SilverWingedSeraph <lfstindall@gmail.com>
2017-06-20 10:54:30 -0700

    Excise incorrect/out of date documentation in root.

--- scamp/scamp.go
+++ scamp/scamp.go
@@ -5,48 +5,10 @@ SCAMP provides SOA bus RPC functionality. Please see root SCAMP/README.md for de
 
 Basics
 
-Services and requesters communicate over persistent TLS connections. First, initialize your environment according to the root README.md. You must have a valid certificate and key to present a service.
+Services and requesters communicate over persistent TLS connections.
+First, initialize your environment according to the root README.md. You must have a valid certificate and key to present a service.
 
-Listening For Requests
-
-  service,err := scamp.NewService(30100)
-  if err != nil {
-	fmt.Printf("could not create service")
-	return
-  }
-  service.Register("helloworld.hello", func(sess *scamp.Session){
-  })
-  service.AcceptSessions()
-
-Making a Request against a Service
-
-  conn,err := scamp.Connect(":30100")
-  if err != nil {
-	fmt.Printf("could not connect to service")
-	return
-  }
-  sess,err := conn.Send(scamp.Request{
-    Action:         "helloworld.hello",
-    EnvelopeFormat: scamp.ENVELOPE_JSON,
-    Version:        1,
-  })
-  if err != nil {
-	fmt.Printf("could not send reqest")
-	return
-  }
-  var reply Reply = sess.Recv()
-
-Library Internals
-
-SCAMP is a layered architecture:
-
-  Request/Reply
-  -------------
-  Session
-  -------------
-  Connection
-  -------------
-  Service
+Every program must call `scamp.Initialize()` before doing anything else, to initialize the global configuration.
 
 */
 package scamp
@@ -57,7 +19,7 @@ import (
 
 var defaultCache *ServiceCache
 
-//Initialize Package-level setup. Right now it just sets up logging.
+//Initialize performs package-level setup. This must be called before calling any other package functionality, as it sets up global configuration.
 func Initialize(configPath string) (err error) {
 	initSCAMPLogger()
 	err = initConfig(configPath)

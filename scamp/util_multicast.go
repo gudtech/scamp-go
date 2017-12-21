d6d1318c4aff0454910dd22ce6348f261e2f4a01
senseijack <senseijack@gmail.com>
2017-12-21 10:20:24 -0800

    made functions and types unexported

--- scamp/util_multicast.go
+++ scamp/util_multicast.go
@@ -5,7 +5,7 @@ import "net"
 
 import "golang.org/x/net/ipv4"
 
-func LoopbackInterface() (lo *net.Interface, err error) {
+func loopbackInterface() (lo *net.Interface, err error) {
 	lo, err = net.InterfaceByName("lo0")
 	if err != nil {
 		lo, err = net.InterfaceByName("lo")
@@ -18,43 +18,11 @@ func LoopbackInterface() (lo *net.Interface, err error) {
 	return
 }
 
-func LocalMulticastPacketConn() (conn *ipv4.PacketConn, err error) {
-	/*
-	  lo,err := LoopbackInterface()
-	  if err != nil {
-	    return
-	  }
-
-	  maddrs, err := lo.MulticastAddrs()
-	  if err != nil {
-	    return
-	  }
-
-	  var bestAddr net.Addr
-	  for _,maddr := range maddrs {
-	    Trace.Printf("looking at: `%s`", maddr.String())
-	    parsedIP := net.ParseIP(maddr.String())
-	    if parsedIP == nil {
-	      Error.Printf("could not parsed IP: `%s`", maddr.String())
-	      continue
-	    } else if parsedIP.To4() == nil {
-	      continue
-	    }
-	    bestAddr = maddr
-	    break
-	  }
-	  if bestAddr == nil {
-	    err = fmt.Errorf("could not find a good address to bind to")
-	    return
-	  }
-
-	  localMulticastSpec := fmt.Sprintf("%s:%d", bestAddr, 5555)
-	*/
-
+func localMulticastPacketConn() (conn *ipv4.PacketConn, err error) {
 	// TODO fundamentally change how multicast is sent. I can't get the API to work
 	// without creating a listener socket first but I shouldn't need it.
 	// Had issues with running multiple services (heka and sdk_service) so I'm
-	// going to the let the OS pick the port. `127.0.0.1:5556` used to work!
+	// going to the let the OS pick the port. `127.0.0.1:5556` used to work! -XRL
 	localMulticastSpec := "127.0.0.1:"
 	Trace.Printf("announce binding to port: `%s`", localMulticastSpec)
 
@@ -69,7 +37,7 @@ func LocalMulticastPacketConn() (conn *ipv4.PacketConn, err error) {
 	return
 }
 
-func IPForAnnouncePacket() (ip net.IP, err error) {
+func getIPForAnnouncePacket() (ip net.IP, err error) {
 	infs, err := net.Interfaces()
 	if err != nil {
 		Error.Printf("err: `%s`", err)

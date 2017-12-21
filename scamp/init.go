4f1f08d9a7128b51ee915a7642d3d53acc8a2ed8
senseijack <senseijack@gmail.com>
2017-12-21 14:56:06 -0800

    moved retry logic to servicecache

--- scamp/init.go
+++ scamp/init.go
@@ -1,14 +1,7 @@
-// Package scamp Copyright 2014 GüdTech, Inc.
-// SCAMP provides SOA bus RPC functionality. Please see root SCAMP/README.md for details on configuring environment.
-// Basics:
-// 	Services and requesters communicate over persistent TLS connections.
-//	First, initialize your environment according to the root README.md. You must have a valid certificate and key to present a service.
-//	Every program must call `scamp.Initialize()` before doing anything else, to initialize the global configuration.
 package scamp
 
 import (
 	"fmt"
-	"time"
 )
 
 var defaultCache *serviceCache
@@ -33,24 +26,4 @@ func Initialize(configPath string) (err error) {
 	}
 
 	return
-}
-
-type stop struct {
-	error
-}
-
-func retry(attempts int, sleep time.Duration, fn func() error) error {
-	if err := fn(); err != nil {
-		if s, ok := err.(stop); ok {
-			// Return the original error for later checking
-			return s.error
-		}
-
-		if attempts--; attempts > 0 {
-			time.Sleep(sleep)
-			return retry(attempts, 2*sleep, fn)
-		}
-		return err
-	}
-	return nil
 }
---
+++ scamp/servicecache.go
@@ -0,0 +1,323 @@
+package scamp
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+type serviceCache struct {
+	path          string
+	cacheM        sync.Mutex
+	identIndex    map[string]*serviceProxy
+	actionIndex   map[string][]*serviceProxy
+	verifyRecords bool
+}
+
+func newServiceCache(path string) (cache *serviceCache, err error) {
+	cache = new(serviceCache)
+	cache.path = path
+
+	cache.identIndex = make(map[string]*serviceProxy)
+	cache.actionIndex = make(map[string][]*serviceProxy)
+	cache.verifyRecords = true
+	//moving this here for now
+	err = cache.Refresh()
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (cache *serviceCache) DisableRecordVerification() {
+	cache.verifyRecords = true
+}
+
+func (cache *serviceCache) EnableRecordVerification() {
+	cache.verifyRecords = false
+}
+
+func (cache *serviceCache) Store(instance *serviceProxy) {
+	cache.cacheM.Lock()
+	defer cache.cacheM.Unlock()
+
+	cache.storeNoLock(instance)
+
+	return
+}
+
+func (cache *serviceCache) storeNoLock(instance *serviceProxy) {
+	_, ok := cache.identIndex[instance.ident]
+	if !ok {
+		cache.identIndex[instance.ident] = instance
+	} else {
+		// Not sure if this is a hard error yet
+		// Error.Printf("tried to store instance that was already tracked")
+		// Override existing version. Correct logic?
+		cache.identIndex[instance.ident] = instance
+	}
+
+	for _, class := range instance.classes {
+		for _, action := range class.actions {
+			for _, protocol := range instance.protocols {
+				mungedName := fmt.Sprintf("%s:%s.%s~%d#%s", instance.sector, class.className, action.actionName, action.version, protocol)
+
+				serviceProxies, ok := cache.actionIndex[mungedName]
+				if ok {
+					serviceProxies = append(serviceProxies, instance)
+				} else {
+					serviceProxies = []*serviceProxy{instance}
+				}
+
+				cache.actionIndex[mungedName] = serviceProxies
+			}
+		}
+	}
+
+	return
+}
+
+func (cache *serviceCache) removeNoLock(instance *serviceProxy) (err error) {
+	_, ok := cache.identIndex[instance.ident]
+	if !ok {
+		err = fmt.Errorf("tried removing an ident which was not being tracked: %s", instance.ident)
+		return
+	}
+
+	delete(cache.identIndex, instance.ident)
+
+	return
+}
+
+// TODO: in a perfect world we'd do upserts to the cache
+// and sweep for stale proxy definitions.
+func (cache *serviceCache) clearNoLock() (err error) {
+	cache.identIndex = make(map[string]*serviceProxy)
+
+	return
+}
+
+func (cache *serviceCache) Retrieve(ident string) (instance *serviceProxy) {
+	cache.cacheM.Lock()
+	defer cache.cacheM.Unlock()
+
+	instance, ok := cache.identIndex[ident]
+	if !ok {
+		instance = nil
+		return
+	}
+
+	return
+}
+
+func (cache *serviceCache) SearchByAction(sector, action string, version int, envelope string) (instances []*serviceProxy) {
+	mungedName := fmt.Sprintf("%s:%s~%d#%s", sector, action, version, envelope)
+
+	return cache.actionIndex[mungedName]
+}
+
+func (cache *serviceCache) SearchByActionWithRetry(sector, action string, version int, envelope string) (instances []*serviceProxy, err error) {
+	mungedName := fmt.Sprintf("%s:%s~%d#%s", sector, action, version, envelope)
+
+	return nil, retry(3, time.Second, func() error {
+		instances = cache.actionIndex[mungedName]
+		if len(instances) == 0 {
+			cache.Refresh()
+			return fmt.Errorf("No service proxy in cache for %s", mungedName)
+		}
+
+		if instances[0] == nil {
+			return stop{fmt.Errorf("service cache contained a nil entry for %s", mungedName)}
+		}
+		return nil
+	})
+}
+
+func (cache *serviceCache) Size() int {
+	cache.cacheM.Lock()
+	defer cache.cacheM.Unlock()
+
+	return len(cache.identIndex)
+}
+
+func (cache *serviceCache) All() (proxies []*serviceProxy) {
+	cache.cacheM.Lock()
+	defer cache.cacheM.Unlock()
+
+	size := len(cache.identIndex)
+	proxies = make([]*serviceProxy, size)
+
+	index := 0
+	for _, proxy := range cache.identIndex {
+		proxies[index] = proxy
+		index++
+	}
+
+	return
+}
+
+var sep = []byte(`%%%`)
+var newline = []byte("\n")
+
+func (cache *serviceCache) Refresh() (err error) {
+	cache.cacheM.Lock()
+	defer cache.cacheM.Unlock()
+
+	stat, err := os.Stat(cache.path)
+	if err != nil {
+		return
+	} else if stat.IsDir() {
+		err = fmt.Errorf("cannot use cache path: `%s` is a directory", cache.path)
+		return
+	}
+	// Trace.Printf("mtime: %s\n", stat.ModTime())
+
+	cacheHandle, err := os.Open(cache.path)
+	if err != nil {
+		return
+	}
+
+	s := bufio.NewScanner(cacheHandle)
+	err = cache.DoScan(s)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (cache *serviceCache) DoScan(s *bufio.Scanner) (err error) {
+	cache.clearNoLock()
+
+	// var entries int = 0
+	// Scan through buf by lines according to this basic ABNF
+	// (SLOP* SEP CLASSRECORD NL CERT NL SIG NL NL)*
+	var classRecordsRaw, certRaw, sigRaw []byte
+	for {
+		var didScan bool
+		for {
+			didScan = s.Scan()
+			if bytes.Equal(s.Bytes(), sep) || !didScan {
+				break
+			}
+		}
+		if !didScan {
+			break
+		}
+		s.Scan() // consume the separator
+
+		if len(s.Bytes()) == 0 {
+			// err = errors.New("unexpected newline after separator")
+			break
+		}
+		classRecordsRaw = make([]byte, len(s.Bytes()))
+		copy(classRecordsRaw, s.Bytes())
+		s.Scan() // consume the classRecords
+
+		if len(s.Bytes()) != 0 {
+			err = errors.New("expected newline after class records")
+			return
+		}
+
+		var certBuffer bytes.Buffer
+		for s.Scan() {
+			// Error.Printf("%s", s.Bytes())
+			if len(s.Bytes()) == 0 {
+				break
+			}
+			certBuffer.Write(s.Bytes())
+			certBuffer.Write(newline)
+		}
+		certRaw = certBuffer.Bytes()[0 : len(certBuffer.Bytes())-1]
+
+		var sigBuffer bytes.Buffer
+		for s.Scan() {
+			// Error.Printf("%s", s.Bytes())
+			if len(s.Bytes()) == 0 {
+				break
+			} else if bytes.Equal(s.Bytes(), sep) {
+				break
+			}
+			sigBuffer.Write(s.Bytes())
+			sigBuffer.Write(newline)
+		}
+		sigRaw = sigBuffer.Bytes()[0 : len(sigBuffer.Bytes())-1]
+
+		// Error.Printf("`%s`", sigRaw)
+
+		// Use those extracted value to make an instance
+		serviceProxy, err := newServiceProxy(classRecordsRaw, certRaw, sigRaw)
+		if err != nil {
+			return fmt.Errorf("newServiceProxy: %s", err)
+		}
+
+		// Validating is a very expensive operation in the benchmarks
+		if cache.verifyRecords {
+			err = serviceProxy.Validate()
+			if err != nil {
+				err = cache.removeNoLock(serviceProxy)
+				if err != nil {
+					Error.Printf("could not remove service proxy (benign on first pass, otherwise it means the service has gone to a bad state): `%s`", err)
+				}
+				continue
+			}
+		}
+
+		cache.storeNoLock(serviceProxy)
+	}
+
+	// fmt.Println("entries:", entries)
+
+	return
+}
+
+var startCert = []byte(`-----BEGIN CERTIFICATE-----`)
+var endCert = []byte(`-----END CERTIFICATE-----`)
+
+func scanCertficates(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	var i int
+
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	// assert cert start line
+	if i = bytes.Index(data, startCert); i == -1 {
+		return 0, nil, nil
+	}
+
+	// assert end line, consume if present
+	if i = bytes.Index(data, endCert); i >= 0 {
+		return i + len(endCert), data[0 : i+len(endCert)], nil
+	}
+	return 0, nil, nil
+}
+
+func retry(attempts int, sleep time.Duration, f func() error) error {
+	if err := f(); err != nil {
+		if s, ok := err.(stop); ok {
+			Error.Println("Retry func exited with err.(stop)")
+			return s.error
+		}
+
+		if attempts--; attempts > 0 {
+			Warning.Printf("remaining retry attempts %v", attempts)
+			jitter := time.Duration(rand.Int63n(int64(sleep)))
+			sleep = sleep + jitter/2
+
+			time.Sleep(sleep)
+			return retry(attempts, 2*sleep, f)
+		}
+		return err
+	}
+	return nil
+}
+
+type stop struct {
+	error
+}

c9b6f562bf0ae1cbe70d7726532596b5a9988f50
senseijack <senseijack@gmail.com>
2018-09-21 16:53:33 -0700

    add file for loading service soa dependencies from file

---
+++ scamp/gdep.go
@@ -0,0 +1,65 @@
+package scamp
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var depfilePath = flag.String("gdep", "./gdep.json", "path to the gdep.json file")
+
+// soaDependencies represents SOA dependencies for the service
+type soaDependencies struct {
+	Offers   []serviceAction `json:"offers"`
+	Requires []soaAction     `json:"requires"`
+}
+
+type soaAction struct {
+	Action string   `json:"action"`
+	Deps   []string `json:"deps"`
+}
+
+type serviceAction struct {
+	Name string `json:"name"`
+}
+
+// ReadDeps reads service dependencies from gdep.yml file
+func readDeps(path string) (deps *soaDependencies, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		err = fmt.Errorf("could not open %s: %s ", path, err)
+		return
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+	if len(data) == 0 {
+		return nil, fmt.Errorf("file data was nil")
+	}
+
+	err = json.Unmarshal([]byte(data), &deps)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func checkRequirement(action soaAction) error {
+	for _, dep := range action.Deps {
+		serviceProxies, err := DefaultCache.SearchByMungedAction(dep)
+		if err != nil {
+			return fmt.Errorf("dependency error: %s", err)
+		}
+		if serviceProxies == nil {
+			return fmt.Errorf("%s not found in discovery cache: %s", dep, err)
+		}
+	}
+
+	return nil
+}
---
+++ scamp/gdep_test.go
@@ -0,0 +1,69 @@
+package scamp
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestReadDeps(t *testing.T) {
+	expected := []byte(`{"offers":[{"name":"main:action1~1"}],"requires":[{"action":"main:action1~1","deps":["main:Logger.info~1#json"]}]}`)
+
+	deps, err := readDeps("./../../scamp-go/fixtures/gdep.json")
+	if err != nil {
+		t.Fatalf("falied to open dep file: %s", err)
+	}
+
+	if deps == nil {
+		t.Fatalf("nil dep struct returned: %s", err)
+	}
+
+	if len(deps.Requires) != 1 {
+		t.Fatalf("expected 2 required soa actions, received %v", len(deps.Requires))
+	}
+	if len(deps.Offers) != 1 {
+		t.Fatalf("expected 2 offered soa actions, received %v", len(deps.Offers))
+	}
+
+	out, err := json.Marshal(deps)
+	if err != nil {
+		t.Fatalf("cannot marshal deps to JSON: %s", err)
+	}
+
+	if string(out) != string(expected) {
+		t.Fatalf("expected: \n%s\n received \n%s\n", string(expected), string(out))
+	}
+
+}
+
+func TestCheckDependencies(t *testing.T) {
+	depPath := "./../../scamp-go/fixtures/gdep.json"
+	depfilePath = &depPath
+	err := Initialize("./../../scamp-go/fixtures/sample_soa.conf")
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	serv, err := NewService("main", "", "sample")
+	if err != nil {
+		t.Fatalf("falied to create service: %s", err)
+	}
+
+	err = serv.CheckDependencies()
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+}
+
+func TestRunningService(t *testing.T) {
+	depPath := "./../../scamp-go/fixtures/gdep.json"
+	depfilePath = &depPath
+	err := Initialize("./../../scamp-go/fixtures/sample_soa.conf")
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	_, err = NewService("main", "", "sample")
+	if err != nil {
+		t.Fatalf("falied to create service: %s", err)
+	}
+}

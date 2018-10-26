6d6050a410d89c65fc30977eb4f5977876ddbe6e
senseijack <senseijack@gmail.com>
2018-10-26 08:57:18 -0700

    moving init functions to service setup

--- scamp/init.go
+++ scamp/init.go
@@ -1,29 +1,25 @@
 package scamp
 
-import (
-	"fmt"
-)
+// Initialize performs package-level setup. This must be called before calling any other package functionality, as it sets up global configuration.
+// TODO: move to package init() function
+// func Initialize(configPath string) (err error) {
+// 	initSCAMPLogger()
+// 	err = initConfig(configPath)
+// 	if err != nil {
+// 		return
+// 	}
 
-var DefaultCache *ServiceCache
+// 	// TODO: move all of this
+// 	cachePath, found := DefaultConfig().Get("discovery.cache_path")
+// 	if !found {
+// 		err = fmt.Errorf("no such config param `discovery.cache_path`. must be set to use scamp-go")
+// 		return
+// 	}
 
-//Initialize performs package-level setup. This must be called before calling any other package functionality, as it sets up global configuration.
-func Initialize(configPath string) (err error) {
-	initSCAMPLogger()
-	err = initConfig(configPath)
-	if err != nil {
-		return
-	}
+// 	DefaultCache, err = NewServiceCache(cachePath)
+// 	if err != nil {
+// 		return
+// 	}
 
-	cachePath, found := DefaultConfig().Get("discovery.cache_path")
-	if !found {
-		err = fmt.Errorf("no such config param `discovery.cache_path`. must be set to use scamp-go")
-		return
-	}
-
-	DefaultCache, err = NewServiceCache(cachePath)
-	if err != nil {
-		return
-	}
-
-	return
-}
+// 	return
+// }

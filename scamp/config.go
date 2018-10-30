dd3e08e872eeee4516a9e73694d132460c48b687
senseijack <senseijack@gmail.com>
2018-10-30 10:22:49 -0700

    refactor

--- scamp/config.go
+++ scamp/config.go
@@ -3,7 +3,6 @@ package scamp
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"regexp"
@@ -22,78 +21,64 @@ var defaultConfig *Config
 var defaultAnnounceInterval = 5
 
 // DefaultConfigPath is the path at which the library will, by default, look for its configuration.
-// var DefaultConfigPath = "/etc/SCAMP/soa.conf"
+var DefaultConfigPath = "/etc/SCAMP/soa.conf"
 
+var configLine = regexp.MustCompile(`^\s*([\S^=]+)\s*=\s*([\S]+)`)
 var globalConfig *Config
 
 var defaultGroupIP = net.IPv4(239, 63, 248, 106)
 var defaultGroupPort = 5555
 
-// initConfig initializes the default scamp config struct and returns an
-// error if it is unable to do so.
 func initConfig(configPath string) (err error) {
-	if len(configPath) == 0 {
-		configPath = defaultConfigPath
-	}
-
-	defaultConfig = &Config{
-		values: make(map[string][]byte),
-	}
-	err = defaultConfig.Load(configPath)
+	defaultConfig = NewConfig()
+	err = DefaultConfig().Load(configPath)
 	if err != nil {
 		err = fmt.Errorf("could not load config: %s", err)
 		return
 	}
-	// TODO: review and possibly remove scamp debugger
+
 	randomDebuggerString = scampDebuggerRandomString()
+
 	return
 }
 
 // NewConfig creates a new configuration struct with default values initialized.
 func NewConfig() (conf *Config) {
-	conf = &Config{
-		values: make(map[string][]byte),
-	}
+	conf = new(Config)
+	conf.values = make(map[string][]byte)
+
 	return
 }
 
 // SetDefaultConfig sets the global configuration manually if need be.
-// In general, users should use Initialize instead.
-// func SetDefaultConfig(conf *Config) {
-// 	initSCAMPLogger()
-// 	defaultConfig = conf
-// }
+func SetDefaultConfig(conf *Config) {
+	defaultConfig = conf
+}
 
 // DefaultConfig fetches the global configuration struct for use.
-// This function panics if the global configuration is not initialized
-// (with `Initialize()`).
+// This function panics if the global configuration is not initialized (with `Initialize()`).
 func DefaultConfig() (conf *Config) {
 	if defaultConfig == nil {
-		log.Fatal("default Config{} is not initialized!")
+		panic("Global configuration defaultConfig is not initialized! Call scamp.Initialize() before using package functionality.")
 	}
 	return defaultConfig
 }
 
 // Load loads configuration k/v pairs from the file at the given path.
-func (conf *Config) Load(configPath string) error {
+func (conf *Config) Load(configPath string) (err error) {
 	file, err := os.Open(configPath)
 	if err != nil {
-		return fmt.Errorf("could not open config file at `%s`: %s", configPath, err)
+		err = fmt.Errorf("couldn't read config from `%s`: %v", configPath, err)
+		return
 	}
-	defer file.Close()
-
 	scanner := bufio.NewScanner(file)
-	err = conf.doLoad(scanner)
-	if err != nil {
-		return fmt.Errorf("couldn't load config info from file: %s", err)
-	}
+	conf.doLoad(scanner)
 
-	return nil
+	return
 }
 
 func (conf *Config) doLoad(scanner *bufio.Scanner) (err error) {
 	var read bool
-	var configLine = regexp.MustCompile(`^\s*([\S^=]+)\s*=\s*([\S]+)`)
 	for {
 		read = scanner.Scan()
 		if !read {
@@ -105,21 +90,27 @@ func (conf *Config) doLoad(scanner *bufio.Scanner) (err error) {
 			conf.values[string(re[1])] = re[2]
 		}
 	}
+
 	return
 }
 
 // ServiceKeyPath uses the configuration to generate a path at which the key for the given service name should be found.
-func (conf *Config) ServiceKeyPath(serviceName string) (keyPath []byte) {
+func (conf *Config) ServiceKeyPath(serviceName, optPath string) []byte {
+	if len(optPath) != 0 {
+		return []byte(fmt.Sprintf("%s/%s.key", optPath, serviceName))
+	}
 	path := conf.values[serviceName+".soa_key"]
 	if path == nil {
-		//TODO: use default paths (and double check with Justin that they are correct)
 		path = []byte(fmt.Sprintf("/etc/GT_private/services/%s.key", serviceName))
 	}
 	return path
 }
 
 // ServiceCertPath uses the configuration to generate a path at which the certificate for the given service name should be found.
-func (conf *Config) ServiceCertPath(serviceName string) (certPath []byte) {
+func (conf *Config) ServiceCertPath(serviceName, optPath string) []byte {
+	if len(optPath) != 0 {
+		return []byte(fmt.Sprintf("%s/%s.crt", optPath, serviceName))
+	}
 	path := conf.values[serviceName+".soa_cert"]
 	if path == nil {
 		path = []byte(fmt.Sprintf("/etc/GT_private/services/%s.crt", serviceName))
@@ -127,9 +118,9 @@ func (conf *Config) ServiceCertPath(serviceName string) (certPath []byte) {
 	return path
 }
 
-// DiscoveryMulticastIP returns the configured discovery address, or the default one
+// discoveryMulticastIP returns the configured discovery address, or the default one
 // if there is no configured address (discovery.multicast_address)
-func (conf *Config) DiscoveryMulticastIP() (ip net.IP) {
+func (conf *Config) discoveryMulticastIP() (ip net.IP) {
 	rawAddr := conf.values["discovery.multicast_address"]
 	if rawAddr != nil {
 		return net.ParseIP(string(rawAddr))
@@ -140,7 +131,7 @@ func (conf *Config) DiscoveryMulticastIP() (ip net.IP) {
 
 // DiscoveryMulticastPort returns the configured discovery port, or the default one
 // if there is no configured port (discovery.port)
-func (conf *Config) DiscoveryMulticastPort() (port int) {
+func (conf *Config) discoveryMulticastPort() (port int) {
 	portBytes := conf.values["discovery.port"]
 	if portBytes != nil {
 		port64, err := strconv.ParseInt(string(portBytes), 10, 0)
@@ -158,7 +149,7 @@ func (conf *Config) DiscoveryMulticastPort() (port int) {
 	return
 }
 
-func (conf *Config) LocalDiscoveryMulticast() bool {
+func (conf *Config) localDiscoveryMulticast() bool {
 	_, ok := conf.values["discovery.local_multicast"]
 	return ok
 }

7462fce721dab92a766355be626348b1510d400a
senseijack <senseijack@gmail.com>
2018-11-02 10:25:41 -0700

    use sync.Once to start and stop announcer

--- scamp/discoveryannounce.go
+++ scamp/discoveryannounce.go
@@ -3,6 +3,7 @@ package scamp
 import (
 	"fmt"
 	"net"
+	"sync"
 	"time"
 
 	"golang.org/x/net/ipv4"
@@ -14,18 +15,23 @@ type DiscoveryAnnouncer struct {
 	multicastConn *ipv4.PacketConn
 	multicastDest *net.UDPAddr
 	stopSig       (chan bool)
+	runOnce       sync.Once
+	stopOnce      sync.Once
+	isStopped     bool
 }
 
 // NewDiscoveryAnnouncer creates a DiscoveryAnnouncer
-func NewDiscoveryAnnouncer() (announcer *DiscoveryAnnouncer, err error) {
-	announcer = new(DiscoveryAnnouncer)
-	announcer.services = make([]*Service, 0, 0)
-	announcer.stopSig = make(chan bool)
+func NewDiscoveryAnnouncer() (a *DiscoveryAnnouncer, err error) {
+	a = new(DiscoveryAnnouncer)
+	a.services = make([]*Service, 0, 0)
+	a.stopSig = make(chan bool)
 
 	config := DefaultConfig()
-	announcer.multicastDest = &net.UDPAddr{IP: config.discoveryMulticastIP(), Port: config.discoveryMulticastPort()}
-	// announcer.multicastDest = &net.UDPAddr{IP: 127.0.0.1, Port: config.DiscoveryMulticastPort()}
-	announcer.multicastConn, err = localMulticastPacketConn(config)
+	a.multicastDest = &net.UDPAddr{
+		IP:   config.discoveryMulticastIP(),
+		Port: config.discoveryMulticastPort(),
+	}
+	a.multicastConn, err = localMulticastPacketConn(config)
 	if err != nil {
 		return
 	}
@@ -34,48 +40,83 @@ func NewDiscoveryAnnouncer() (announcer *DiscoveryAnnouncer, err error) {
 }
 
 // Stop notifies stopSig channel to stop announcer
-func (announcer *DiscoveryAnnouncer) Stop() {
-	announcer.stopSig <- true
+func (a *DiscoveryAnnouncer) Stop() {
+	a.stopOnce.Do(func() {
+		a.stopSig <- true
+		a.stopped()
+	})
 }
 
-// Track indicates that announcer should track and announce service
-func (announcer *DiscoveryAnnouncer) Track(s *Service) {
-	announcer.services = append(announcer.services, s)
+// Track indicates that announcer should track and announce the service
+func (a *DiscoveryAnnouncer) Track(s *Service) {
+	a.services = append(a.services, s)
+}
+
+func (a *DiscoveryAnnouncer) start() {
+	var wg sync.WaitGroup
+	a.runOnce.Do(func() {
+		wg.Add(1)
+		go func() {
+			//TODO: should we use waitgroup here?
+			a.isStopped = false
+			a.announceLoop()
+			wg.Done()
+		}()
+	})
+	wg.Wait()
+	Error.Println("exiting announcer.start()")
 }
 
 // AnnounceLoop runs service announceloop and runs announcer.doAnnounce() at time
 // interval configured in defaultAnnounceInterval
-// TODO: make defaultAnnounceInterval configurable in the service (rather than hardcoded in scamp)
-func (announcer *DiscoveryAnnouncer) AnnounceLoop() {
-	// Trace.Printf("starting announcer loop")
-
+// TODO: update to detect when announceloop fails and announcer isn;t actually announcing
+func (a *DiscoveryAnnouncer) announceLoop() {
+	defer a.stopped()
+announceloop:
 	for {
 		select {
-		case <-announcer.stopSig:
-			return
+		case <-a.stopSig:
+			break announceloop
 		default:
-			err := announcer.doAnnounce()
+			err := a.doAnnounce()
 			if err != nil {
-				// do something
+				Error.Fatalf("doAnnounce error: %s\n", err)
+				break announceloop
 			}
 		}
 
 		time.Sleep(time.Duration(defaultAnnounceInterval) * time.Second)
 	}
+	Warning.Println("exiting announceLoop")
 }
 
-func (announcer *DiscoveryAnnouncer) doAnnounce() (err error) {
-	for _, s := range announcer.services {
-		serviceDesc, err := s.MarshalText()
-		if err != nil {
-			return fmt.Errorf("Failed to marshal service as text: `%s`. skipping", err)
+func (a *DiscoveryAnnouncer) doAnnounce() (err error) {
+	if len(a.services) == 0 {
+		panic("service len 0")
+	}
+	for _, s := range a.services {
+		var serviceDesc []byte
+		// TODO: store serviceDesc
+		if s.remarshalForAnnounce {
+			serviceDesc, err = s.MarshalText()
+			if err != nil {
+				return fmt.Errorf("Failed to marshal service as text: `%s`. skipping", err)
+			}
+			s.mu.Lock()
+			s.announceBytes = serviceDesc
+			s.remarshalForAnnounce = false
+			s.mu.Unlock()
 		}
 
-		_, err = announcer.multicastConn.WriteTo(serviceDesc, nil, announcer.multicastDest)
+		_, err = a.multicastConn.WriteTo(s.announceBytes, nil, a.multicastDest)
 		if err != nil {
 			return fmt.Errorf("failed writing to multicastConn: %s", err)
 		}
 	}
 
 	return
+}
+
+func (a *DiscoveryAnnouncer) stopped() {
+	a.isStopped = true
 }

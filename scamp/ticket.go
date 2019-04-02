25a88eb373d90932991197c03c123ba3ec52eff6
Aceeri <conmcclusk@gmail.com>
2019-04-02 12:00:29 -0700

    Lock on service cache, verify tickets/privs on actions

--- scamp/ticket.go
+++ scamp/ticket.go
@@ -1,113 +1,126 @@
 package scamp
 
-import "bytes"
-import "errors"
-import "strconv"
+import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
 
-import "encoding/pem"
-import "crypto/rsa"
-import "crypto/x509"
+var verifyKey *rsa.PublicKey
+var readVerifyKey sync.Once
 
-// Ticket represents a scamp auth ticket
+// Ticket represents an SOA ticket
 type Ticket struct {
-	Version       int64
-	UserID        int64
-	ClientID      int64
-	ValidityStart int64
-	ValidityEnd   int64
-	TTL           int
-	Expired       bool
+	Version   int
+	UserID    int
+	ClientID  int
+	Timestamp int64
+	// TTL = Time To Live
+	TTL        int64
+	Privileges map[int]bool
 }
 
-var separator = []byte(",")
-var supportedVersion = []byte("1")
+var defaultKeyPath = "/etc/GT/auth/ticket_verify_public_key.pem"
 
-func readTicket(incoming []byte, signingPubKey []byte) (ticket Ticket, err error) {
-	rsaPubKey, err := parseRsaPubKey(signingPubKey)
-	if err != nil {
-		return
+func VerifyTicket(unparsedTicket string, keyPath string) (*Ticket, error) {
+	if len(keyPath) == 0 {
+		keyPath = defaultKeyPath
 	}
 
-	ticketBytes, signature := splitTicketPayload(incoming)
-
-	err = verifySHA256(ticketBytes, rsaPubKey, signature, true)
-	if err != nil {
-		return
+	parts := strings.Split(strings.TrimSpace(unparsedTicket), ",")
+	if len(parts) < 6 {
+		return nil, fmt.Errorf("ticket missing parts, wanted 6 parts, have %d", len(parts))
 	}
 
-	ticket, err = parseTicketBytes(ticketBytes)
-	if err != nil {
-		return
+	sig := parts[len(parts)-1]
+	parts = parts[:len(parts)-1]
+
+	readVerifyKey.Do(func() {
+		vkStr, err := ioutil.ReadFile(keyPath)
+		if err != nil {
+			panic(err)
+		}
+
+		block, _ := pem.Decode(vkStr)
+		if block == nil {
+			panic("can't decode nil pem")
+		}
+
+		pk, _ := x509.ParsePKIXPublicKey(block.Bytes)
+		verifyKey = pk.(*rsa.PublicKey)
+	})
+
+	if verifyKey == nil {
+		return nil, fmt.Errorf("`%s` verify key not readable", keyPath)
 	}
 
-	return
+	signature, err := base64.RawURLEncoding.DecodeString(sig)
+	if err != nil {
+		return nil, fmt.Errorf("decode signature: %s", err)
+	}
+
+	hashed := sha256.Sum256([]byte(strings.Join(parts, ",")))
+
+	verifyErr := rsa.VerifyPKCS1v15(verifyKey, crypto.SHA256, hashed[:], signature)
+	if verifyErr != nil {
+		return nil, fmt.Errorf("unable to verify ticket: %s", verifyErr)
+	}
+
+	if parts[0] != "1" { // version
+		return nil, fmt.Errorf("invalid version")
+	}
+
+	var ticket Ticket
+	ticket.Version = 1
+
+	ticket.UserID, err = strconv.Atoi(parts[1])
+	if err != nil {
+		return nil, fmt.Errorf("parse user id (%v): %s", parts[1], err)
+	}
+
+	ticket.ClientID, err = strconv.Atoi(parts[2])
+	if err != nil {
+		return nil, fmt.Errorf("parse client id (%v): %s", parts[2], err)
+	}
+
+	ticket.Timestamp, err = strconv.ParseInt(parts[3], 10, 0)
+	if err != nil {
+		return nil, fmt.Errorf("parse timestamp (%v): %s", parts[3], err)
+	}
+
+	ticket.TTL, err = strconv.ParseInt(parts[4], 10, 0)
+	if err != nil {
+		return nil, fmt.Errorf("parse TTL: %s", err)
+	}
+
+	if len(parts) > 5 {
+		ticket.Privileges = make(map[int]bool)
+		for _, priv := range strings.Split(parts[5], "+") {
+			priv, err := strconv.Atoi(priv)
+			if err != nil {
+				return nil, fmt.Errorf("parse priv (%v): %s", priv, err)
+			}
+			ticket.Privileges[priv] = true
+		}
+	}
+
+	if ticket.Expired() {
+		return nil, fmt.Errorf("ticket expired")
+	}
+
+	return &ticket, nil
 }
 
-func readTicketNoVerify(incoming []byte) (ticket Ticket, err error) {
-	ticketBytes, _ := splitTicketPayload(incoming)
-	return parseTicketBytes(ticketBytes)
-}
-
-func splitTicketPayload(incoming []byte) (ticketBytes []byte, ticketSig []byte) {
-	lastIndex := bytes.LastIndex(incoming, separator)
-	ticketBytes = incoming[:lastIndex]
-	ticketSig = incoming[lastIndex+1:]
-	return
-}
-
-func parseRsaPubKey(signingPubKey []byte) (rsaPubKey *rsa.PublicKey, err error) {
-	block, _ := pem.Decode(signingPubKey)
-	if block == nil {
-		err = errors.New("expected valid block")
-	}
-
-	key, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return
-	}
-
-	rsaPubKey, ok := key.(*rsa.PublicKey)
-	if !ok {
-		err = errors.New("could not cast parsed value to rsa.PublicKey")
-		return
-	}
-
-	return
-}
-
-func parseTicketBytes(ticketBytes []byte) (ticket Ticket, err error) {
-	chunks := bytes.Split(ticketBytes, separator)
-
-	if !bytes.Equal(chunks[0], supportedVersion) {
-		err = errors.New("ticket must be version 1")
-		return
-	}
-
-	ticket.Version, err = strconv.ParseInt(string(chunks[0]), 10, 0)
-	if err != nil {
-		return
-	}
-
-	ticket.UserID, err = strconv.ParseInt(string(chunks[1]), 10, 0)
-	if err != nil {
-		return
-	}
-
-	ticket.ClientID, err = strconv.ParseInt(string(chunks[2]), 10, 0)
-	if err != nil {
-		return
-	}
-
-	ticket.ValidityStart, err = strconv.ParseInt(string(chunks[3]), 10, 0)
-	if err != nil {
-		return
-	}
-
-	validityDuration, err := strconv.ParseInt(string(chunks[4]), 10, 0)
-	if err != nil {
-		return
-	}
-	ticket.ValidityEnd = ticket.ValidityStart + validityDuration
-
-	return
+func (ticket *Ticket) Expired() bool {
+	expiryDate := ticket.Timestamp + ticket.TTL
+	return expiryDate < time.Now().Unix()
 }

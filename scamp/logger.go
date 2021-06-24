ddd0a8eeb0c2e19676fe70456e49dfecc2a15093
Justin Schell <justin@gudtech.com>
2021-06-24 08:29:07 -0700

    Adjust log formatting

--- scamp/logger.go
+++ scamp/logger.go
@@ -1,8 +1,8 @@
 package scamp
 
 import (
-	"os"
 	"log"
+	"os"
 )
 
 var (
@@ -28,7 +28,7 @@ func initSCAMPLogger() {
 
 	Trace = log.New(new(nullWriter), "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
 	// Trace = log.New(os.Stdout, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(os.Stdout, "INFO:  ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(os.Stdout, "WARN:  ", log.Ldate|log.Ltime|log.Lshortfile)
 	Error = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
